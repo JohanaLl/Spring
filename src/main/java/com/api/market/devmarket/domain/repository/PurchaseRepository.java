d2178c3696478fa60a8cbc28bd9371644c1371ed
JohanaLl <johisllanos@gmail.com>
2023-08-07 19:38:38 -0500

    Se crea el modelo del dominio para compras

---
+++ src/main/java/com/api/market/devmarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.api.market.devmarket.domain.repository;
+
+import com.api.market.devmarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
